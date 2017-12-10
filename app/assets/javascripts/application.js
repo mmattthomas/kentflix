bcb3699be2101d5826dd2359c2b0a5c80a8f0479
Matt Thomas <mmattthomas@gmail.com>
2017-12-09 18:59:39 -0800

    fixing bootstrap dropdown menu

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery3
-//= require rails-ujs
+// require jquery3
+// require rails-ujs
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
