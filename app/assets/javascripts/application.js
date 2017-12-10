7a069839ab9ab5647644817d994a8f875ef335f5
Matt Thomas <mmattthomas@gmail.com>
2017-12-09 18:32:45 -0800

    fixing assets part 1.2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery3
 //= require rails-ujs
 //= require turbolinks
-//= require jquery3
 //= require_tree .
